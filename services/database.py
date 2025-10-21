4e59881b4b8ddad607460ae6cabe11dd304db48b
Eram <erammemon392@gmail.com>
2025-10-21 23:03:36 +0530

    Add main project scripts and database module

---
+++ services/database.py
@@ -0,0 +1,43 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+# -------------------------
+# MongoDB connection
+# -------------------------
+MONGO_URI = "mongodb+srv://healthuser:eram1205@cluster0.ink2ock.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URI)
+
+db = client["healthcare_ai"]
+collection = db["responses"]
+
+# -------------------------
+# Save a QA pair
+# -------------------------
+def save_response(question, answer, model_name="t5-small", timestamp=None):
+    """
+    Save a question-answer pair to MongoDB.
+    
+    :param question: User question
+    :param answer: Model-generated answer
+    :param model_name: Name of the model used
+    :param timestamp: Optional timestamp, defaults to current time
+    """
+    if timestamp is None:
+        timestamp = datetime.utcnow()
+
+    doc = {
+        "question": question,
+        "answer": answer,
+        "model": model_name,
+        "timestamp": timestamp
+    }
+    collection.insert_one(doc)
+
+# -------------------------
+# Retrieve all responses
+# -------------------------
+def get_all_responses():
+    """
+    Fetch all question-answer pairs from the database.
+    """
+    return list(collection.find({}, {"_id": 0}))
